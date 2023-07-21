b39ed6bfb206af104b171ddc7610128e070538c7
shemil <shemilkaweesha@gmail.com>
2023-07-21 21:45:05 +0530

    Basic CRUD Operations in User Service

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -5,8 +5,35 @@ import { User } from './user.entity';
 
 @Injectable()
 export class UsersService {
+  //  Injecting the User Repository
+  constructor(@InjectRepository(User) private repo: Repository<User>) {}
 
-    //  Injecting the User Repository
-    constructor(@InjectRepository(User) private repo : Repository<User>){}
+  create(email: string, password: string) {
+    const user = this.repo.create({ email, password }); // Create only make an instance it does not save the user here
+    return this.repo.save(user); // Saving the created user
+  }
 
+  findOne(id: number) {
+    return this.repo.findOneBy({ id });
+  }
+
+  find(email: string) {
+    return this.repo.find({ where: { email } });
+  }
+  async update(id: number, attrs: Partial<User>) {
+    const user = await this.findOne(id);
+    if (!user) {
+      throw new Error('User not found with ID : ' + id);
+    }
+    Object.assign(user, attrs);
+    this.repo.save(user);
+  }
+
+  async remove(id:number) {
+    const user = await this.findOne(id);
+    if (!user) {
+      throw new Error('User not found with ID : ' + id);
+    }
+    this.repo.remove(user);
+  }
 }
