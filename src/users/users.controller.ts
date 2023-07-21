4833bbca8ec77fbfa213f9ea85a8ba5a4c1bd052
shemil <shemilkaweesha@gmail.com>
2023-07-21 22:15:16 +0530

    Connected controller with services in User

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,12 +1,35 @@
-import { Body, Controller , Post} from '@nestjs/common';
+import { Body, Controller, Post, Get, Patch,Param,Query, Delete } from '@nestjs/common';
 import { CreateUserDto } from './dtos/create-user.dto';
+import { UsersService } from './users.service';
+import { UpdateUserDto } from './dtos/update-user.dto';
 
 @Controller('auth')
 export class UsersController {
+  constructor(private service: UsersService) {}
 
-    @Post("/signup")
-    createUser(@Body() user : CreateUserDto) : CreateUserDto{
-        return user;
-    }
+  @Post('/signup')
+  createUser(@Body() user: CreateUserDto) {
+    return this.service.create(user.email, user.password);
+  }
+
+  @Get("/:id")
+  findUser(@Param('id') id:string){
+    return this.service.findOne(parseInt(id))
+  }
+
+  @Get()
+  findAllUsers(@Query('email') email: string){
+    return this.service.find(email);
+  }
+
+  @Delete("/:id")
+  deleteUser(@Param('id') id: string){
+    return this.service.remove(parseInt(id));
+  }
+
+  @Patch("/:id")
+  updateUser(@Param('id') id: string, @Body() user : UpdateUserDto){
+    return this.service.update(parseInt(id),user);
+  }
 
 }
