65555a77db8d4f8db0ea5aea8bc879f935eb8be3
shemil <shemilkaweesha@gmail.com>
2023-07-27 22:00:56 +0530

    Using Cookie Sessions in the main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,14 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
-
+const cookieSession = require('cookie-session'); 
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
+  app.use(cookieSession({
+    keys: ['randomletters'], // These letters are used to encrypt the cookies
+  }))
   app.useGlobalPipes(
     new ValidationPipe({
       whitelist: true
