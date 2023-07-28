edf80130fb2af3d4ebaa2c41939c509b2e1b7047
shemil <shemilkaweesha@gmail.com>
2023-07-28 22:27:16 +0530

    Creating a new param decorator

--- src/users/decorators/current-user.decorator.ts
+++ src/users/decorators/current-user.decorator.ts
@@ -1,5 +1,15 @@
 import { createParamDecorator, ExecutionContext } from "@nestjs/common";
 
-export const CurrentUser = createParamDecorator((data:any, context: ExecutionContext)=> {
-    
-})
+export const CurrentUser = createParamDecorator(
+
+    // Param decorators exist outside the DI system, so our decorator can't get an instance of UsersService directly
+
+    (data:never, context: ExecutionContext)=> {
+        const request = context.switchToHttp().getRequest();
+        if(request.session?.userId){
+
+            // Accessing the CurrentUser property setuped in the interceptor
+            return request.currentUser;
+        } else return null;
+    }
+)
