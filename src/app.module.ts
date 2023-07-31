0ab4ec30e9edb0c292f5d2d7fc16bbe991316dc4
shemil <shemilkaweesha@gmail.com>
2023-07-31 12:26:25 +0530

    Usage of .env files and env config

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,14 +6,24 @@ import { ReportsModule } from './reports/reports.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { User } from './users/user.entity';
 import { Report } from './reports/report.entity';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 
 @Module({
   imports: [
-    TypeOrmModule.forRoot({
-      type: 'sqlite',
-      database: 'db.sqlite',
-      entities: [User, Report], // Setting up the entities
-      synchronize: true,
+    TypeOrmModule.forRootAsync({
+      inject: [ConfigService],
+      useFactory: (config: ConfigService) =>{
+        return {
+          type: 'sqlite',
+          database: config.get('DB_NAME'),
+          entities: [User, Report], // Setting up the entities
+          synchronize: true,
+        }
+      }
+    }),
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: `.env.${process.env.NODE_ENV}`
     }),
     UsersModule,
     ReportsModule,
