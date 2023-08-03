9439175efd84162f7afec318bac9eb29c2f1ad58
shemil <shemilkaweesha@gmail.com>
2023-08-03 20:58:19 +0530

    Change approve status in a report : Patch

--- src/reports/reports.controller.ts
+++ src/reports/reports.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller,Post,UseGuards } from '@nestjs/common';
+import { Body, Controller,Post,Patch,Param,UseGuards } from '@nestjs/common';
 import { CreateReportDto } from './dtos/create-report.dto';
 import { ReportsService } from './reports.service';
 import { AuthGuard } from 'src/gaurds/auth.guard';
@@ -6,6 +6,7 @@ import { CurrentUser } from 'src/users/decorators/current-user.decorator';
 import { User } from 'src/users/user.entity';
 import { ReportDto } from './dtos/report.dto';
 import { Serialize } from 'src/interceptors/serialize.interceptor';
+import { ApproveReportDto } from './dtos/approve-report.dto';
 
 @Controller('reports')
 @UseGuards(AuthGuard)
@@ -17,6 +18,11 @@ export class ReportsController {
     @Serialize(ReportDto)
     createReport(@Body() body: CreateReportDto, @CurrentUser() user: User ){
         return this.reportService.create(body,user)
+    }
+
+    @Patch('/:id')
+    approveReport(@Param('id') id:string, @Body() body: ApproveReportDto ){
+        return this.reportService.changeApproval(id, body.approved)
     }
 
 }
