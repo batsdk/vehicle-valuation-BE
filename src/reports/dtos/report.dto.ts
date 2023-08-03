1b4ce4ddb5f36cdab08904afefcc2684438b7068
shemil <shemilkaweesha@gmail.com>
2023-08-03 19:56:57 +0530

    Creating Report Dto

---
+++ src/reports/dtos/report.dto.ts
@@ -0,0 +1,27 @@
+import { Expose, Transform } from 'class-transformer';
+import { User } from 'src/users/user.entity';
+
+export class ReportDto {
+  @Expose() // Expose means the response can see this
+  id: number;
+  @Expose()
+  price: number;
+  @Expose()
+  year: number;
+  @Expose()
+  lng: number;
+  @Expose()
+  lat: number;
+  @Expose()
+  make: string;
+  @Expose()
+  model: string;
+  @Expose()
+  mileage: number;
+  //   Obj -> Original report entity
+  @Transform(({obj: report})=> report.user.id )
+//   Above : Saying get the original report object and set its user id to the below value and expose it
+  @Expose()
+  userId: number;
+
+}
