e3e61cfee5f10bd638ec41af4cddb331476269a7
shemil <shemilkaweesha@gmail.com>
2023-08-07 22:06:25 +0530

    Transforming the DTO using class transformer

--- src/reports/dtos/get-estimate.dto.ts
+++ src/reports/dtos/get-estimate.dto.ts
@@ -1,3 +1,4 @@
+import { Transform } from 'class-transformer';
 import {
   IsLatitude,
   IsLongitude,
@@ -14,19 +15,24 @@ export class GetEstimateDto {
   @IsString()
   model: string;
 
+//   Value means the value we are working on right now
+  @Transform(({value})=> parseInt(value))
   @IsNumber()
   @Min(1930)
   @Max(2025)
   year: number;
 
+  @Transform(({value}) => parseInt(value))
   @IsNumber()
   @Min(0)
   @Max(1000000)
   mileage: number;
 
+  @Transform(({value}) => parseFloat(value))
   @IsLongitude()
   lng: number;
 
+  @Transform(({value}) => parseFloat(value))
   @IsLatitude()
   lat: number;
 
