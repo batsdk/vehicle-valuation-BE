6dff7720a899b1c01c4cf80ecc9a395333235818
shemil <shemilkaweesha@gmail.com>
2023-08-07 21:59:11 +0530

    Get Estimate Method

--- src/reports/reports.controller.ts
+++ src/reports/reports.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller,Post,Patch,Param,UseGuards } from '@nestjs/common';
+import { Body, Controller,Post,Patch,Param,UseGuards, Get, Query } from '@nestjs/common';
 import { CreateReportDto } from './dtos/create-report.dto';
 import { ReportsService } from './reports.service';
 import { AuthGuard } from 'src/gaurds/auth.guard';
@@ -8,12 +8,18 @@ import { ReportDto } from './dtos/report.dto';
 import { Serialize } from 'src/interceptors/serialize.interceptor';
 import { ApproveReportDto } from './dtos/approve-report.dto';
 import { AdminGaurd } from 'src/gaurds/admin.gaurd';
+import { GetEstimateDto } from './dtos/get-estimate.dto';
 
 @Controller('reports')
 @UseGuards(AuthGuard)
 export class ReportsController {
 
     constructor(private reportService: ReportsService){}
+
+    @Get()
+    getEstimate(@Query() query: GetEstimateDto){
+        return "hi";
+    }
 
     @Post()
     @Serialize(ReportDto)
